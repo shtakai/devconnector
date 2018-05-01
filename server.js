6d6df3946cdf48ef384200c31c55839b46966611
Alyson T <shtakai@gmail.com>
2018-05-01 21:30:04 +0700

    Modify accept json

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require('dotenv-safe').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
