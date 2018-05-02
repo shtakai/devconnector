45437afdc5b6359423b56664b7c1b198b3e2b530
Alyson T <shtakai@gmail.com>
2018-05-02 21:54:01 +0700

    Add create JWT token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -60,11 +63,26 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then((isMatch) => {
           if (isMatch) {
-            res.json({ msg: 'Success' });
+            // User matched
+            const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: `Bearer ${token}`,
+                });
+              },
+            );
           } else {
             return res.status(400).json({ password: 'Password incorrect' });
           }
         });
     });
 });
+
 module.exports = router;
